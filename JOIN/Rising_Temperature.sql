e4e14be7c83eb7851d615b72c754b1f9518147d4
Mathiyarasu18 <138567222+Mathiyarasu18@users.noreply.github.com>
2024-07-19 11:26:34 +0530

    Create Rising_Temperature.sql

---
+++ JOIN/Rising_Temperature.sql
@@ -0,0 +1,28 @@
+SELECT w1.id
+FROM Weather w1
+JOIN Weather w2 
+ON w1.recordDate = DATE_ADD(w2.recordDate,INTERVAL 1 DAY)
+WHERE w1.temperature > w2.temperature;
+
+/*
+Input: 
+Weather table:
++----+------------+-------------+
+| id | recordDate | temperature |
++----+------------+-------------+
+| 1  | 2015-01-01 | 10          |
+| 2  | 2015-01-02 | 25          |
+| 3  | 2015-01-03 | 20          |
+| 4  | 2015-01-04 | 30          |
++----+------------+-------------+
+Output: 
++----+
+| id |
++----+
+| 2  |
+| 4  |
++----+
+Explanation: 
+In 2015-01-02, the temperature was higher than the previous day (10 -> 25).
+In 2015-01-04, the temperature was higher than the previous day (20 -> 30).
+*/
