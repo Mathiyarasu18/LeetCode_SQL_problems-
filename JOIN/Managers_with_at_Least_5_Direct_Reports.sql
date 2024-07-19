b788135f910898b2c5039aac951152b163157c53
Mathiyarasu18 <138567222+Mathiyarasu18@users.noreply.github.com>
2024-07-19 11:38:32 +0530

    Create Managers_with_at_Least_5_Direct_Reports.sql

---
+++ JOIN/Managers_with_at_Least_5_Direct_Reports.sql
@@ -0,0 +1,28 @@
+SELECT name
+FROM Employee
+WHERE id IN(
+    SELECT managerId FROM Employee 
+    GROUP BY managerId 
+    HAVING COUNT(managerId)>=5
+    );
+
+/*
+Input: 
+Employee table:
++-----+-------+------------+-----------+
+| id  | name  | department | managerId |
++-----+-------+------------+-----------+
+| 101 | John  | A          | null      |
+| 102 | Dan   | A          | 101       |
+| 103 | James | A          | 101       |
+| 104 | Amy   | A          | 101       |
+| 105 | Anne  | A          | 101       |
+| 106 | Ron   | B          | 101       |
++-----+-------+------------+-----------+
+Output: 
++------+
+| name |
++------+
+| John |
++------+
+*/
