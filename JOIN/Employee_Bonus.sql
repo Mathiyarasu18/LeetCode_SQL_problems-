746cfe1bcd5635d74d5eec0bc29236e036f55838
Mathiyarasu18 <138567222+Mathiyarasu18@users.noreply.github.com>
2024-07-19 11:32:18 +0530

    Create Employee_Bonus.sql

---
+++ JOIN/Employee_Bonus.sql
@@ -0,0 +1,33 @@
+SELECT name, bonus 
+FROM Employee
+LEFT JOIN Bonus 
+ON Employee.empId = Bonus.empId 
+WHERE Bonus.bonus<1000 OR Bonus IS NULL;
+
+/*
+Input: 
+Employee table:
++-------+--------+------------+--------+
+| empId | name   | supervisor | salary |
++-------+--------+------------+--------+
+| 3     | Brad   | null       | 4000   |
+| 1     | John   | 3          | 1000   |
+| 2     | Dan    | 3          | 2000   |
+| 4     | Thomas | 3          | 4000   |
++-------+--------+------------+--------+
+Bonus table:
++-------+-------+
+| empId | bonus |
++-------+-------+
+| 2     | 500   |
+| 4     | 2000  |
++-------+-------+
+Output: 
++------+-------+
+| name | bonus |
++------+-------+
+| Brad | null  |
+| John | null  |
+| Dan  | 500   |
++------+-------+
+*/
