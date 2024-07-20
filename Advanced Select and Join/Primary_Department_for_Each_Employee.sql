a495b9acbf44fb0e37981aeeac43f2b439d0e041
Mathiyarasu18 <138567222+Mathiyarasu18@users.noreply.github.com>
2024-07-20 18:22:00 +0530

    Create Primary_Department_for_Each_Employee.sql

---
+++ Advanced Select and Join/Primary_Department_for_Each_Employee.sql
@@ -0,0 +1,37 @@
+SELECT employee_id, department_id
+FROM Employee
+WHERE primary_flag='Y' OR employee_id IN(
+    SELECT employee_id FROM Employee
+    GROUP BY employee_id
+    HAVING COUNT(employee_id)=1
+);
+
+/*
+Input: 
+Employee table:
++-------------+---------------+--------------+
+| employee_id | department_id | primary_flag |
++-------------+---------------+--------------+
+| 1           | 1             | N            |
+| 2           | 1             | Y            |
+| 2           | 2             | N            |
+| 3           | 3             | N            |
+| 4           | 2             | N            |
+| 4           | 3             | Y            |
+| 4           | 4             | N            |
++-------------+---------------+--------------+
+Output: 
++-------------+---------------+
+| employee_id | department_id |
++-------------+---------------+
+| 1           | 1             |
+| 2           | 1             |
+| 3           | 3             |
+| 4           | 3             |
++-------------+---------------+
+Explanation: 
+- The Primary department for employee 1 is 1.
+- The Primary department for employee 2 is 1.
+- The Primary department for employee 3 is 3.
+- The Primary department for employee 4 is 3.
+*/
