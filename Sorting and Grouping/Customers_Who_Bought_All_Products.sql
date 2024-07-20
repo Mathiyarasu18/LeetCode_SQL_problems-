60ea2d35886c7e87eb8c0cd912cff375c9464cc5
Mathiyarasu18 <138567222+Mathiyarasu18@users.noreply.github.com>
2024-07-20 13:09:32 +0530

    Create Customers_Who_Bought_All_Products.sql

---
+++ Sorting and Grouping/Customers_Who_Bought_All_Products.sql
@@ -0,0 +1,37 @@
+SELECT customer_id
+FROM Customer
+GROUP BY customer_id
+HAVING COUNT(DISTINCT product_key)= (
+    SELECT COUNT(*) 
+    FROM Product
+);
+
+/*
+Input: 
+Customer table:
++-------------+-------------+
+| customer_id | product_key |
++-------------+-------------+
+| 1           | 5           |
+| 2           | 6           |
+| 3           | 5           |
+| 3           | 6           |
+| 1           | 6           |
++-------------+-------------+
+Product table:
++-------------+
+| product_key |
++-------------+
+| 5           |
+| 6           |
++-------------+
+Output: 
++-------------+
+| customer_id |
++-------------+
+| 1           |
+| 3           |
++-------------+
+Explanation: 
+The customers who bought all the products (5 and 6) are customers with IDs 1 and 3.
+*/
