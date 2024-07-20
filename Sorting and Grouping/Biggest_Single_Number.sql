db19ff859d53e8ae9364abfdec6666c7b1ea29ef
Mathiyarasu18 <138567222+Mathiyarasu18@users.noreply.github.com>
2024-07-20 12:53:46 +0530

    Create Biggest_Single_Number.sql

---
+++ Sorting and Grouping/Biggest_Single_Number.sql
@@ -0,0 +1,32 @@
+SELECT MAX(num) AS num
+FROM MyNumbers
+WHERE num IN(
+    SELECT num FROM MyNumbers
+    GROUP BY num
+    HAVING COUNT(num)=1
+);
+
+/*
+Input: 
+MyNumbers table:
++-----+
+| num |
++-----+
+| 8   |
+| 8   |
+| 3   |
+| 3   |
+| 1   |
+| 4   |
+| 5   |
+| 6   |
++-----+
+Output: 
++-----+
+| num |
++-----+
+| 6   |
++-----+
+Explanation: The single numbers are 1, 4, 5, and 6.
+Since 6 is the largest single number, we return it.
+*/
