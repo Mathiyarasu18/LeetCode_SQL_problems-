8fb16ba95a368fd9d14d0d17328081a5366172bd
Mathiyarasu18 <138567222+Mathiyarasu18@users.noreply.github.com>
2024-07-22 13:21:17 +0530

    Create Last_Person_to_Fit_in_the_Bus.sql

---
+++ Advanced Select and Join/Last_Person_to_Fit_in_the_Bus.sql
@@ -0,0 +1,40 @@
+SELECT q1.person_name
+FROM Queue q1
+JOIN Queue q2
+ON q1.turn >=q2.turn 
+GROUP BY q1.turn
+HAVING SUM(q2.weight) <=1000
+ORDER BY SUM(q2.weight) DESC
+LIMIT 1;
+
+/*
+Input: 
+Queue table:
++-----------+-------------+--------+------+
+| person_id | person_name | weight | turn |
++-----------+-------------+--------+------+
+| 5         | Alice       | 250    | 1    |
+| 4         | Bob         | 175    | 5    |
+| 3         | Alex        | 350    | 2    |
+| 6         | John Cena   | 400    | 3    |
+| 1         | Winston     | 500    | 6    |
+| 2         | Marie       | 200    | 4    |
++-----------+-------------+--------+------+
+Output: 
++-------------+
+| person_name |
++-------------+
+| John Cena   |
++-------------+
+Explanation: The folowing table is ordered by the turn for simplicity.
++------+----+-----------+--------+--------------+
+| Turn | ID | Name      | Weight | Total Weight |
++------+----+-----------+--------+--------------+
+| 1    | 5  | Alice     | 250    | 250          |
+| 2    | 3  | Alex      | 350    | 600          |
+| 3    | 6  | John Cena | 400    | 1000         | (last person to board)
+| 4    | 2  | Marie     | 200    | 1200         | (cannot board)
+| 5    | 4  | Bob       | 175    | ___          |
+| 6    | 1  | Winston   | 500    | ___          |
++------+----+-----------+--------+--------------+
+*/
