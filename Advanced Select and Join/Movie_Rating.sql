ac0d4069c95e3dc24f35a1c18f7e893d76fb5962
Mathiyarasu18 <138567222+Mathiyarasu18@users.noreply.github.com>
2024-07-23 10:34:25 +0530

    Create  Movie_Rating.sql

---
+++ Advanced Select and Join/Movie_Rating.sql
@@ -0,0 +1,58 @@
+(SELECT DISTINCT name AS results 
+FROM MOvieRating m1
+JOIN Users u ON m1.user_id = u.user_id
+GROUP BY m1.user_id
+ORDER BY COUNT(rating) DESC,name LIMIT 1)
+UNION ALL
+(SELECT title AS results
+FROM MovieRating m1
+JOIN Movies m2 ON m1.movie_id = m2.movie_id
+WHERE YEAR(created_at) ='2020' AND MONTH(created_at) ='02'
+GROUP BY title
+ORDER BY AVG(m1.rating) DESC,
+title LIMIT 1);
+
+/*
+Input: 
+Movies table:
++-------------+--------------+
+| movie_id    |  title       |
++-------------+--------------+
+| 1           | Avengers     |
+| 2           | Frozen 2     |
+| 3           | Joker        |
++-------------+--------------+
+Users table:
++-------------+--------------+
+| user_id     |  name        |
++-------------+--------------+
+| 1           | Daniel       |
+| 2           | Monica       |
+| 3           | Maria        |
+| 4           | James        |
++-------------+--------------+
+MovieRating table:
++-------------+--------------+--------------+-------------+
+| movie_id    | user_id      | rating       | created_at  |
++-------------+--------------+--------------+-------------+
+| 1           | 1            | 3            | 2020-01-12  |
+| 1           | 2            | 4            | 2020-02-11  |
+| 1           | 3            | 2            | 2020-02-12  |
+| 1           | 4            | 1            | 2020-01-01  |
+| 2           | 1            | 5            | 2020-02-17  | 
+| 2           | 2            | 2            | 2020-02-01  | 
+| 2           | 3            | 2            | 2020-03-01  |
+| 3           | 1            | 3            | 2020-02-22  | 
+| 3           | 2            | 4            | 2020-02-25  | 
++-------------+--------------+--------------+-------------+
+Output: 
++--------------+
+| results      |
++--------------+
+| Daniel       |
+| Frozen 2     |
++--------------+
+Explanation: 
+Daniel and Monica have rated 3 movies ("Avengers", "Frozen 2" and "Joker") but Daniel is smaller lexicographically.
+Frozen 2 and Joker have a rating average of 3.5 in February but Frozen 2 is smaller lexicographically.
+*/
