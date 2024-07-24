bb94fd1fabaa9f6d5fd0c8425a583504e68f0410
Mathiyarasu18 <138567222+Mathiyarasu18@users.noreply.github.com>
2024-07-24 14:47:06 +0530

    Create Friend_Requests_II_Who_Has_the_Most_Friends.sql

---
+++ Advanced Select and Join/Friend_Requests_II_Who_Has_the_Most_Friends.sql
@@ -0,0 +1,30 @@
+WITH main AS(
+    SELECT accepter_id AS id FROM RequestAccepted
+    UNION ALL
+    SELECT requester_id AS id FROM RequestAccepted
+)
+SELECT id, COUNT(*) AS num FROM main 
+GROUP BY 1
+ORDER BY 2 DESC
+LIMIT 1;
+
+/*
+Input: 
+RequestAccepted table:
++--------------+-------------+-------------+
+| requester_id | accepter_id | accept_date |
++--------------+-------------+-------------+
+| 1            | 2           | 2016/06/03  |
+| 1            | 3           | 2016/06/08  |
+| 2            | 3           | 2016/06/08  |
+| 3            | 4           | 2016/06/09  |
++--------------+-------------+-------------+
+Output: 
++----+-----+
+| id | num |
++----+-----+
+| 3  | 3   |
++----+-----+
+Explanation: 
+The person with id 3 is a friend of people 1, 2, and 4, so he has three friends in total, which is the most number than any others.
+*/
